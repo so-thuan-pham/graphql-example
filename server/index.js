610d7ad6fe97d91e1169cad7495bc6c777047cff
so-thuan-pham <35915268+so-thuan-pham@users.noreply.github.com>
2018-12-11 14:36:10 +0700

    Add RootQuery RootMutation

--- server/index.js
+++ server/index.js
@@ -1,39 +1,80 @@
 //https://medium.com/@lachlanmiller_52885/graphql-basics-and-practical-examples-with-vue-6b649b9685e0
 
-const express = require('express')
-const { graphql, buildSchema } = require('graphql')
-const graphqlHTTP = require('express-graphql')
-const cors = require('cors')
+const express = require('express');
+const { graphql, buildSchema } = require('graphql');
+const graphqlHTTP = require('express-graphql');
+const cors = require('cors');
 
-const Champion = require('./champion.js')
+const Champion = require('./champion.js');
 
 const champions = [
     new Champion('Ashe', 100),
     new Champion('Vayne', 200)
-]
+];
+
+const events = [];
 
 const schema = buildSchema(`
-    type Query {
+    type RootQuery {
         language: String
         getChampions: [Champion]
+        events: [Event!]!
+    }
+
+    type RootMutation {
+        createEvent(eventInput: EventInput) : Event
     }
 
     type Champion {
         name: String
         attackDamage: Float
     }
-`)
+
+    type Event {
+        _id: ID
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+    }
+
+    input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+    }
+
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+`);
 
 const rootValue = {
     language: () => 'GraphQL',
-    getChampions: () => champions
-}
+    getChampions: () => champions,
+    events: () => events,
+    createEvent: args => {
+        const event = {
+            _id: Math.random().toString(),
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: args.eventInput.price,
+            date: args.eventInput.date
+        };
+        events.push(event);
+        return event;
+    }
+};
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
-    rootValue, schema, graphiql: true
-}))
+    rootValue, 
+    schema, 
+    graphiql: true
+}));
 
-app.listen(4000, () => console.log('Listening on 4000'))
+app.listen(4000, () => console.log('Listening on 4000'));
