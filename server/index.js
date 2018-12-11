9fc2c11e1a0a2d5ebe12a09908325f8c6d64502f
so-thuan-pham <35915268+so-thuan-pham@users.noreply.github.com>
2018-12-11 14:01:47 +0700

    Custom Types with Models

--- server/index.js
+++ server/index.js
@@ -5,14 +5,28 @@ const { graphql, buildSchema } = require('graphql')
 const graphqlHTTP = require('express-graphql')
 const cors = require('cors')
 
+const Champion = require('./champion.js')
+
+const champions = [
+    new Champion('Ashe', 100),
+    new Champion('Vayne', 200)
+]
+
 const schema = buildSchema(`
     type Query {
         language: String
+        getChampions: [Champion]
+    }
+
+    type Champion {
+        name: String
+        attackDamage: Float
     }
 `)
 
 const rootValue = {
-    language: () => 'GraphQL'
+    language: () => 'GraphQL',
+    getChampions: () => champions
 }
 
 const app = express()
